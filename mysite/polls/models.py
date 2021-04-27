295ff397cbf5f6053f3451e711930b866beb96f2
Cameron <cmnorris7@gmail.com>
2021-04-26 21:41:29 -0400

    Finished tutorial part 3. Set up urls & views

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,31 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+"""
+A model contains the essential fields & behaviors of the stored data; they are how we define the data.
+Three stsp for editing a model:
+---change model in models.py
+---Run 'python manage.py makemigrations' to create migrations for changes
+---Run 'python manage.py migrate' to apply changes to database
+"""
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now()
+datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
