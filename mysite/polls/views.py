09993f58438c4b032f5e59e3d33367f0b1ffa990
Cameron <cmnorris7@gmail.com>
2021-05-03 20:51:34 -0400

    fixed packages in views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.template import loader
 from .models import Question
 
