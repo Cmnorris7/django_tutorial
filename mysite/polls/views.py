9883a2b34946bc1b90917098c282ebef79757b9a
Cameron <cmnorris7@gmail.com>
2021-05-03 20:58:58 -0400

    One more views.py library fix

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.template import loader
-from .models import Question
+from .models import Choice, Question
 
 
 def index(request):
